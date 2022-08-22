1df708811515237802880a452c125b83f5ba09a1
matthew.earlywine <matthew.earlywine@AFSWWK47C9MT97.us.accenturefederal.com>
2022-08-22 09:28:00 -0500

    Completed conversion to Spring enabled "conference-java"
    from basic java coded "conference"

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,10 +8,16 @@ import com.pluralsight.repository.SpeakerRepository;
 
 public class SpeakerServiceImpl implements SpeakerService {
 	
-	private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+	private SpeakerRepository repository;
 
 	public List<Speaker> findAll(){
 		return repository.findAll();
 	}
+
+	public void setRepository(SpeakerRepository repository) {
+		this.repository = repository;
+	}
+	
+	
 	
 }
